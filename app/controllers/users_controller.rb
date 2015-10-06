fd88f5da1288385a0b95a6c227c761ac043500be
gptasinski <g.ptasinski78@gmail.com>
2015-10-05 19:28:10 -0500

    add initial session pages - not functioning

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to 'films/index'
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,36 +3,37 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  validates :hashed_password, presence: true
+  validates :password_digest, presence: true
 
-  validate :password_length
+  # validate :password_length
 
   has_many :comments, foreign_key: "commenter_id"
   has_many :reviews, foreign_key: "reviewer_id"
   has_many :ratings, foreign_key: "rater_id"
+  has_secure_password
 
-  def password
-    @password ||= Password.new(hashed_password)
-  end
+  # def password
+  #   @password ||= Password.new(password_digest)
+  # end
 
-  def password=(new_password)
-    @raw_password = new_password
-    self.hashed_password = @password = Password.create(new_password)
-  end
+  # def password=(new_password)
+  #   @raw_password = new_password
+  #   self.hashed_password = @password = Password.create(password_digest)
+  # end
 
-  def self.authenticate(user_data)
-    user = User.find_by(email: user_data[:email])
-    user if user && user.password == user_data[:password]
-  end
+  # def self.authenticate(user_data)
+  #   user = User.find_by(email: user_data[:email])
+  #   user if user && user.password == user_data[:password]
+  # end
 
-  private
+  # private
 
-  def password_length
-    if @raw_password || new_record?
-      unless @raw_password && @raw_password.length != 0
-        errors.add(:password, "cannot be empty")
-      end
-    end
-  end
+  # def password_length
+  #   if @raw_password || new_record?
+  #     unless @raw_password && @raw_password.length != 0
+  #       errors.add(:password, "cannot be empty")
+  #     end
+  #   end
+  # end
 
 end
