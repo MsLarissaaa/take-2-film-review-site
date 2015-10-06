e52a174e86c8b596dd47116da211011d97824585
MsLarissaaa <larissapittenger@gmail.com>
2015-10-06 11:21:01 -0500

    Add reviews and comments to finish database seed file

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ActiveRecord::Base
-  belongs_to :reviewer, class_name: :Reviewer
+  belongs_to :reviewer, class_name: :User
   belongs_to :film
+  has_many :comments
 
   has_many :ratings, as: :rateable
 
