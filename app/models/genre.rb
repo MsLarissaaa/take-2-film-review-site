88a2fadb30b3a90e0505dc718515e13119735f3a
MsLarissaaa <larissapittenger@gmail.com>
2015-10-06 22:45:48 -0500

    Add best movies view

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,7 +3,6 @@ class Genre < ActiveRecord::Base
   has_many :films, through: :film_genres
 
   def self.make_slices(genre)
-    # genre_type = self.find_by_title(genre)
     sorted_movies = genre.films.sort_by{ |film| film.release_date }.reverse
     rows = (sorted_movies.length == 4 ? 1 : sorted_movies.length % 4)
     Array.new(rows) {sorted_movies.shift(4)}
